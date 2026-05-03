d64c975b9c744b16a8b481979a3cda87467eec8b
Jonas Gomes de Lima Neto <jotajonas09@gmail.com>
2026-05-03 15:18:18 -0300

    Editar README.md, editar package.json e comentar src/server.ts

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
-Esta API é feita somente para fim de testes e não é um produto real, além disso ainda há páginas faltando e funções incompletas.
+Esta API é feita somente para fim de testes e não é um produto real, há inclusive funções ainda não implementadas durante a criação deste README.
 
-A marca FastLibrary apresentada neste site é fictícia.
+O código, logo e empresa do website foram criados por mim. A empresa no website é totalmente fictícia.
+
+Módulos instalados:
+- Express (e @types/express)
+- Typescript (e @types/typescript)
+- TSC
+- EJS (e @types/ejs)
+- PATH
+
+
+
+Jonas Gomes de Lima Neto, 3 de Maio de 2026 [14:44].
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
-// Importando
+// ------------------------------------------------------------------- //
+//                            Configuração                             //
+// ------------------------------------------------------------------- //
 
+// Import
 import { time } from "console"
 import express, {Request, Response} from "express"
 import path from "path"
@@ -9,13 +12,14 @@ import { textSpanOverlap } from "typescript"
 
 // Configuração
 const app = express()
-const PORT = 6767
+const PORT = 3000
 const HOST = '0.0.0.0'
 
 app.use(express.json())
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.urlencoded({ extended: true }));
 
+// Logger Middleware
 app.use((req, res, next) => {
     const agora = new Date().toLocaleTimeString()
 
@@ -23,11 +27,17 @@ app.use((req, res, next) => {
     next()
 })
 
+// EJS
 app.set("view engine", "ejs")
 app.set("views", "src/views")
 
-// Variáveis
 
+
+// ------------------------------------------------------------------- //
+//                            Variáveis                                //
+// ------------------------------------------------------------------- //
+
+// Livro
 interface Livro {
     id: Number
     titulo: String
@@ -36,16 +46,18 @@ interface Livro {
     disponivel?: Boolean
 }
 
+// /lista.json, onde estão os livros adicionados com POST.
 const dadosLoja = "lista.json"
 
 // Leitura do arquivo lista.json.
 async function readLivros(): Promise<Livro[]> {
     try {
         const texto = await readFile(dadosLoja, "utf-8");
-        return JSON.parse(texto) as Livro[];
+        console.log(`Dados carregados com sucesso!`)
+        return JSON.parse(texto) as Livro[]; // Array
     }
     catch {
-        console.log(`O arquivo ${dadosLoja} ainda não existe.`)
+        console.log(`O arquivo "${dadosLoja}" não foi encontrado na pasta do projeto, adicione um livro em http://${HOST}:${PORT}/lista/adicionar para criar o arquivo ou o importe`)
         return [];
   }
 }
@@ -55,50 +67,54 @@ async function writeLivros(lista: Livro[]) {
     await writeFile(dadosLoja, JSON.stringify(lista, null, 2));
 }
 
+
+
+// ------------------------------------------------------------------- //
+//                             Programa                                //
+// ------------------------------------------------------------------- //
+
+// Carregar os livros presentes em lista.json
 readLivros()
 
-
-
-
-
-// Página Inicial
+// GET Página Inicial
 app.get("/", async (req, res) => {
     res.render("index")
 })
 
-// Saiba mais
+// GET Saiba mais
 app.get("/sobre", async (req, res) => {
     res.render("saibamais")
 })
 
-// Loja
+// GET Loja de livros
 app.get("/lista", async (req, res) => {
     const loja = await readLivros()
     try {
         res.render("lista", {lista:loja})
     }
     catch {
-        res.status(500).json({sucesso: false, status: "Erro interno do servidor"})
-        console.log("Falha ao acessar banco de dados")
+        res.status(500).json({sucesso: false, status: "Erro interno do servidor ao tentar executar a rota GET"})
+        console.log("(/lista) Falha ao acessar banco de dados")
     }
 })
 
+// GET Loja de livros (em JSON)
 app.get("/lista/json", async (req, res) => {
     const loja = await readLivros()
     try {
         res.json(loja)
     } catch {
-        res.status(500).json({sucesso: false, status: "Erro interno do servidor"})
-        console.log("Falha ao acessar banco de dados")
+        res.status(500).json({sucesso: false, status: "Erro interno do servidor ao tentar executar a rota GET"})
+        console.log("(/lista/json) Falha ao acessar banco de dados")
     }
 })
 
-// Adicionar Produto (Página)
+// GET Página de adicionar produto
 app.get("/lista/adicionar", async (req, res) => {
     res.render("adicionar")
 })
 
-// Adicionar Produto (Rota POST)
+// POST Adição do produto
 app.post("/lista/adicionar", async (req, res) => {
 
     const {titulo, ano, genero} = req.body
@@ -110,7 +126,7 @@ app.post("/lista/adicionar", async (req, res) => {
 
         const novoLivro = {id: nextID, titulo, ano, genero, disponivel: disponibilidade}
 
-
+        // Validação
         const erros: String[] = []
         if (typeof titulo !== "string" || !titulo) {
             erros.push("O nome é obrigatório!")
@@ -126,34 +142,45 @@ app.post("/lista/adicionar", async (req, res) => {
         await writeLivros(loja)
         res.redirect("/sucesso")
     } catch {
-        res.status(500).json({sucesso: false, mensagem: "Erro interno do servidor"})
+        res.status(500).json({sucesso: false, mensagem: "Erro interno do servidor ao tentar executar a rota POST"})
+        console.log("(/lista/adicionar) Falha ao adicionar produto")
     }
 })
 
-// Sucesso ao adicionar
+// GET Sucesso ao adicionar produto na rota POST /lista/adicionar
 app.get("/sucesso", async (req, res) => {
     try {
     const loja = await readLivros()
-    const ultimoValor = Number(loja.length-1)
+    const ultimoValor = Number(loja.length-1) // É pra ser o item adicionado após o POST.
     const ultimoAdd = loja[ultimoValor]
 
     res.render("sucesso", {livro:ultimoAdd})
     } catch {
-        res.status(500).json({sucesso: false, mensagem: "Erro interno do servidor"})
+        res.status(500).json({sucesso: false, mensagem: "Erro interno do servidor ao tentar executar rota GET"})
+        console.log("(/sucesso) Falha ao carregar página de sucesso")
     }
 })
 
-// Página do livro
+// GET Livro específico (JSON)
 app.get("/lista/:id", async (req, res) => {
-    const loja = await readLivros()
     const ID = Number(req.params.id)
+    try {
+    const loja = await readLivros()
     const busca = loja.find(a => a.id === ID)
 
-    if (busca) {res.render("livro", {livro:busca})} 
-    else {res.status(404).render("livroerro", {ID})}
+    if (busca) {res.json(busca)} 
+    else {res.status(404).json({sucesso: false, mensagem: `Não há nenhum produto com o ID ${ID}`})}
+    } catch {
+        res.status(500).json({sucesso: false, mensagem: "Erro interno do servidor ao tentar executar rota GET"})
+        console.log(`(/lista/:id) Falha ao tentar acessar o livro com ID ${ID}`)
+    }
 })
+
+// ------------------------------------------------------------------- //
+//                             Execução                                //
+// ------------------------------------------------------------------- //
 
 // Listener
 app.listen(PORT, HOST, () => {
-    console.log(`Connected! Hosted at: http://${HOST}:${PORT}`)
+    console.log(`Conectado na rede local! Acesse neste dispositivo em: http://${HOST}:${PORT} | Para acessar em outros dispositivos escreva o IP local IPv4 da máquina em que o servidor está sendo executado ao invés de '0.0.0.0'`)
 })
